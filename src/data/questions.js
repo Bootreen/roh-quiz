82bab7a9981bc91a7a734fb5b051490ca3498238
HENNING KÖNITZ <koenitz92@gmx.de>
2024-06-26 12:58:53 +0200

    create data file questions.js

---
+++ src/data/questions.js
@@ -0,0 +1,50 @@
+export const quizQuestions = [
+  {
+    question: "Wofür steht CSS?",
+    answers: [
+      "Computer Style Sheets",
+      "Creative Style Sheets",
+      "Cascading Style Sheets",
+      "Colored Style Sheets",
+    ],
+  },
+  {
+    question: "Welche CSS Syntax ist korrekt?",
+    answers: [
+      "{body: color=black;}",
+      "{body; color: black;}",
+      "body {color: black;}",
+      "body: color=black;",
+    ],
+  },
+  {
+    question: 'Wie selektiert man ein Element mit id="demo"?',
+    answers: ["#demo", ".demo", "demo", "*demo"],
+  },
+  {
+    question: "Wie selektiert man alle p Elemente in einem div?",
+    answers: ["div + p", "div p", "div p.all", "div.p"],
+  },
+  {
+    question: "Was ist der Defaultwert für position?",
+    answers: ["relative", "fixed", "absolute", "static"],
+  },
+  {
+    question: "Wie deklariert man eine Funktion in JavaScript?",
+    answers: [
+      "function myFunction()",
+      "function:myFunction()",
+      "function=myFunction()",
+      "const myFunction()",
+    ],
+  },
+  {
+    question: "Wie ruft man eine Funktion in JavaScript auf?",
+    answers: [
+      "call myFunction()",
+      "myFunction.call()",
+      "myFunction()",
+      "() => { myFunction() }",
+    ],
+  },
+];
