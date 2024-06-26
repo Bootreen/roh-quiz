8d64c463d52bea3d91ae162e7119d82225bf7dbc
HENNING KÖNITZ <koenitz92@gmx.de>
2024-06-26 14:52:36 +0200

    initial text and button of the homepage

--- src/components/PageHome.jsx
+++ src/components/PageHome.jsx
@@ -1,6 +1,14 @@
 export const PageHome = () => (
   <>
     <h2>ROH Quiz</h2>
-    <h3>Homepage</h3>
+    <h3>Welcome! </h3>
+    <p>
+      Test your coding knowledge! Click through the questions and select the
+      correct answer. You can continue by clicking the Next button. You can see
+      your progress in the quiz at the top. At the end you will find out whether
+      you are already a coding pro or whether you still have a lot to learn...
+      Have fun!
+    </p>
+    <button onClick={onSwitchPage}>Next</button>
   </>
 );
