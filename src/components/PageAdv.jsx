5acf78490c41985da5c7e1b7b933332724adafd4
Rainer Braun <rainerbraun@MacBook-Pro-von-Rainer.fritz.box>
2024-06-27 16:29:39 +0200

    AdvPageadded

--- src/components/PageAdv.jsx
+++ src/components/PageAdv.jsx
@@ -1,6 +1,18 @@
+import "./PageAdv.css";
+
 export const PageAdv = () => (
   <>
     <h2>ROH Quiz</h2>
     <h3>Advertising</h3>
+    <div className="imageLink">
+      <a
+        href="https://talents.taktsoft.com/bewerbung?bootcamp=0"
+        target="_blank"
+      >
+        <img src="/TaktsoftLogo.jpg" />
+      </a>
+    </div>
   </>
 );
+
+export default PageAdv;
