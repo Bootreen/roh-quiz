7a0d23ce5b91516171191a1f0f54a49e954e1f45
Oleksii Butrin <bootreen@gmail.com>
2024-06-27 12:19:51 +0200

    Solved extra rerendering problem when choosing right option

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { HOME, QUIZ, ADV, RESULT } from "./data/pages";
+import { shuffle } from "./utils/shuffle";
 import { PageHome } from "./components/PageHome";
 import { PageQuiz } from "./components/PageQuiz";
 import { PageAdv } from "./components/PageAdv";
@@ -8,13 +9,30 @@ import { PageResult } from "./components/PageResult";
 
 const App = () => {
   const [currPage, setCurrPage] = useState(HOME);
+  const [correctAnswers, setCorrectAnswers] = useState(0);
+  const [shuffled, setShuffled] = useState();
+
+  useEffect(() => {
+    setShuffled(shuffle(7));
+  }, []);
+
   return (
     <>
       {currPage === HOME && <PageHome navHandler={() => setCurrPage(QUIZ)} />}
-      {currPage === QUIZ && <PageQuiz navHandler={() => setCurrPage(ADV)} />}
+      {currPage === QUIZ && (
+        <PageQuiz
+          navHandler={() => setCurrPage(ADV)}
+          answersHandler={setCorrectAnswers}
+          correctAnswers={correctAnswers}
+          shuffled={shuffled}
+        />
+      )}
       {currPage === ADV && <PageAdv navHandler={() => setCurrPage(RESULT)} />}
       {currPage === RESULT && (
-        <PageResult navHandler={() => setCurrPage(HOME)} />
+        <PageResult
+          navHandler={() => setCurrPage(HOME)}
+          correctAnswers={correctAnswers}
+        />
       )}
     </>
   );
--- src/components/PageQuiz.jsx
+++ src/components/PageQuiz.jsx
@@ -1,8 +1,14 @@
 import "./PageQuiz.css";
 import { useState } from "react";
 import { quizQuestions } from "../data/questions";
+// import { shuffle } from "../utils/shuffle";
 
-export const PageQuiz = () => {
+export const PageQuiz = ({
+  navHandler,
+  answersHandler,
+  correctAnswers,
+  shuffled,
+}) => {
   const [currQuestion, setCurrQuestion] = useState(0);
   const { question, options } = quizQuestions[currQuestion];
   const switchQuestion = () =>
@@ -10,13 +16,42 @@ export const PageQuiz = () => {
       currQuestion === quizQuestions.length - 1 ? 0 : currQuestion + 1
     );
 
+  // Completed
+  // 1. Make a copy of options array
+  // 2. Shuffle the copy
+  // 3. Render shuffled copy to the screen
+  // 4. During render conditionally assign onClick event handler to options buttons
+  // 5. For the right answer onClick has to call answersHandler and increment correctAnswers
+  // 6. For the wrong answer onClick has to do nothing (for now)
+
+  // 8. Prevent additional options shuffle after the answer
+
+  // ToDo
+  // 7. Prohibit multiply answers for the same question
+  //    7.1 - Introduce new state - isAnswered
+  //    7.2 - When isAnswered, make options buttons disabled and highlight right answer by green color
+  //    7.3 - When isAnswered AND wrong option was selected, additioonaly highlight wrong answer by red
+  //    7.4 - Next Question button has to be disabled if NOT isAnswered
+
   return (
     <>
+      <h1>Correct answers: {correctAnswers}</h1>
       <h2>Question {currQuestion + 1}</h2>
       <h3>{question}</h3>
       <div className='options-container'>
-        {options.map((option, id) => (
-          <button key={id}>{option}</button>
+        {shuffled[currQuestion].map((id) => (
+          <button
+            key={id}
+            onClick={
+              options[id] === options[0]
+                ? () => {
+                    answersHandler(correctAnswers + 1);
+                  }
+                : () => {}
+            }
+          >
+            {options[id]}
+          </button>
         ))}
       </div>
       <button onClick={switchQuestion}>Next Question</button>
