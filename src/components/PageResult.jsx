ed2ed3ff9b8168435e244ee09ea30e2681b02738
Oleksii Butrin <bootreen@gmail.com>
2024-06-27 16:53:39 +0200

    Version 0.1 aplha

--- src/components/PageResult.jsx
+++ src/components/PageResult.jsx
@@ -1,7 +1,8 @@
-export const PageResult = ({ correctAnswers }) => (
+export const PageResult = ({ navHandler, correctAnswers }) => (
   <>
     <h2>ROH Quiz</h2>
     <h3>Your result</h3>
     <p>Du hast {correctAnswers} richtig beantwortet.</p>
+    <button onClick={navHandler}>Homepage</button>
   </>
 );
