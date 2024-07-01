978f4424eeffb027a4004e7f99ed2ffe5ea5cd46
Rainer Braun <rainerbraun@MacBook-Pro-von-Rainer.local>
2024-07-01 11:33:21 +0200

    ProgressBar

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,11 +1,24 @@
-import { quizQuestions } from "../data/questions";
+import "./ProgressBar.css";
+import { clsx } from "clsx";
 
+import { quizQuestions } from "../data/questions";
 export const ProgressBar = ({ currQuestion }) => {
+  const renderProgress = () => {
+    let content = [];
+    for (let i = 0; i < quizQuestions.length; i++)
+      content.push(
+        <div
+          className={clsx("progress-element", i <= currQuestion && "active")}
+        ></div>
+      );
+    return content;
+  };
   return (
     <>
       <div>
         {currQuestion + 1} of {quizQuestions.length}
       </div>
+      <div className="progress-container">{renderProgress()}</div>
     </>
   );
 };
