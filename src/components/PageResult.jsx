0eff4c8e8913b896fef532f48757afa3adfadcb3
HENNING KÖNITZ <koenitz92@gmx.de>
2024-07-01 14:51:16 +0200

    add gifs

--- src/components/PageResult.jsx
+++ src/components/PageResult.jsx
@@ -4,11 +4,30 @@ export const PageResult = ({ navHandler, correctAnswers, answersHandler }) => {
     navHandler();
   };
 
+  const resultImages = [
+    "/zero.gif",
+    "/dissapointment.gif",
+    "/dissapointment.gif",
+    "/not-bad.gif",
+    "/not-bad.gif",
+    "/nod.gif",
+    "/cheers.gif",
+    "/excited.gif",
+  ];
+
   return (
     <>
       <h2>ROH Quiz</h2>
       <h3>Your result</h3>
-      <p>Du hast {correctAnswers} Fragen richtig beantwortet.</p>
+      <div>
+        {correctAnswers === 1 && <p>You answered 1 question correctly.</p>}
+        {correctAnswers > 1 && correctAnswers <= 7 && (
+          <p>You answered {correctAnswers} questions correctly.</p>
+        )}
+      </div>
+
+      <img src={resultImages[correctAnswers]} alt="Ergebnis-GIF" />
+      <br />
       <button onClick={restartHandler}>Homepage</button>
     </>
   );
